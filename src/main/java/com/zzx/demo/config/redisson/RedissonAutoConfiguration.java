ee5e2e8bf8c55c00018183cd3c840525859c494c
zhzhxian <zhzhxian@163.com>
2019-04-06 22:11:46 +0800

    删除哨兵模式自动装配和单机模式自动装配

--- src/main/java/com/zzx/demo/config/redisson/RedissonAutoConfiguration.java
+++ src/main/java/com/zzx/demo/config/redisson/RedissonAutoConfiguration.java
@@ -33,48 +33,6 @@ public class RedissonAutoConfiguration {
     private RedissonProperties redssionProperties;
 
     /**
-     * 哨兵模式自动装配
-     *
-     * @return
-     */
-//    @Bean
-//    @ConditionalOnProperty(name = "redisson.master-name")
-//    RedissonClient redissonSentinel() {
-//        Config config = new Config();
-//        SentinelServersConfig serverConfig = config.useSentinelServers()
-//                .addSentinelAddress(redssionProperties.getSentinelAddresses())
-//                .setMasterName(redssionProperties.getMasterName()).setTimeout(redssionProperties.getTimeout())
-//                .setMasterConnectionPoolSize(redssionProperties.getMasterConnectionPoolSize())
-//                .setSlaveConnectionPoolSize(redssionProperties.getSlaveConnectionPoolSize());
-//
-//        if (StringUtils.isNotBlank(redssionProperties.getPassword())) {
-//            serverConfig.setPassword(redssionProperties.getPassword());
-//        }
-//        return Redisson.create(config);
-//    }
-
-    /**
-     * 单机模式自动装配
-     *
-     * @return
-     */
-//    @Bean
-//    @ConditionalOnProperty(name = "redisson.address")
-//    RedissonClient redissonSingle() {
-//        Config config = new Config();
-//        SingleServerConfig serverConfig = config.useSingleServer().setAddress(redssionProperties.getAddress())
-//                .setTimeout(redssionProperties.getTimeout())
-//                .setConnectionPoolSize(redssionProperties.getConnectionPoolSize())
-//                .setConnectionMinimumIdleSize(redssionProperties.getConnectionMinimumIdleSize());
-//
-//        if (StringUtils.isNotBlank(redssionProperties.getPassword())) {
-//            serverConfig.setPassword(redssionProperties.getPassword());
-//        }
-//
-//        return Redisson.create(config);
-//    }
-
-    /**
      * 装配locker类，并将实例注入到RedissLockUtil中
      *
      * @return
